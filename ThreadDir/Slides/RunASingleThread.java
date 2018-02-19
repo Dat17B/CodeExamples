f07f27de0ad2d34ec0e6e3c6cc3d32cc2218fbcf
Troels H. Jensen <lord_ni@hotmail.com>
2018-02-19 17:48:53 +0100

    Video code example with objects

---
+++ ThreadDir/Slides/RunASingleThread.java
@@ -0,0 +1,19 @@
+public class RunASingleThread
+{
+    public static void main(String[] args)
+    {
+        // Creating an object of Thread
+        // These two threads have no implemented run method
+        Thread ordinaryThread = new Thread();
+        Thread namedThread = new Thread("Some name");
+        // This thread have a run method specified in the MyThread class
+        Thread myThread = new MyThread();
+        // These two threads is injecting the class MyRunnable(that implements the Runnable interface)
+        // run method specified in the MyRunnable class
+        Thread myRunnable = new Thread(new MyRunnable());
+        Thread myNamedRunnable = new Thread(new MyRunnable(), "Some runnable thread name");
+
+        // Start thread
+        ordinaryThread.start();
+    }
+}
