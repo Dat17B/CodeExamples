c7d82502822d0098671496e074b86bc2e0b25d32
Troels H. Jensen <lord_ni@hotmail.com>
2018-02-19 17:39:19 +0100

    Exceptions commit

---
+++ Exceptions/ExerciseFiles/AgeValidationException.java
@@ -0,0 +1,7 @@
+package Dat15v2;
+
+/**
+ * Created by lyng on 20/04/15.
+ */
+public class AgeValidationException extends Exception {
+}
---
+++ Exceptions/ExerciseFiles/OhNoException.java
@@ -0,0 +1,12 @@
+package Dat15v2;
+
+/**
+ * Created by lyng on 20/04/15.
+ */
+public class OhNoException extends ArithmeticException {
+
+    public void message()
+    {
+        System.out.print("Oh no");
+    }
+}
